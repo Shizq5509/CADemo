c6b2556fe3fa94d8dc9568ac32bd92a22fc77b88
zhengqian.shi <zhengqian.shi@qunar.com>
2017-02-15 10:57:28 +0800

    EmitterLayerTest

--- AnimationTest/LayerSubClassTest/EmitterLayerTestVC.m
+++ AnimationTest/LayerSubClassTest/EmitterLayerTestVC.m
@@ -21,12 +21,34 @@
     
     [self.view setBackgroundColor:[UIColor blackColor]];
     
+    [self initEmitter];
+    
     [self makeSnow];
 }
 
 - (void)makeSnow
 {
+    CAEmitterCell *snowCell = [CAEmitterCell emitterCell];
+    snowCell.contents = (__bridge id _Nullable)([UIImage imageNamed:@"snow.png"].CGImage);
+    snowCell.birthRate = 5;
+    snowCell.lifetime = 20;
+    snowCell.velocity = 100;
+    snowCell.velocityRange = 50;
+    snowCell.emissionLongitude = M_PI_2;
+    snowCell.emissionRange = M_PI_2;
+    snowCell.scaleRange = 0.5;
+    
+    _testLayer.emitterCells = @[snowCell];
+}
 
+- (void)initEmitter
+{
+    _testLayer = [CAEmitterLayer layer];
+    _testLayer.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    _testLayer.renderMode = kCAEmitterLayerAdditive;
+    _testLayer.emitterPosition = CGPointMake(self.view.frame.size.width/2, -35);
+    _testLayer.emitterSize = CGSizeMake(self.view.frame.size.width, 20);
+    [self.view.layer addSublayer:_testLayer];
 }
 
 @end
